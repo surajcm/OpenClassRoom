803e44d6db3e8d4b0b2c10d46df32c494bf2e67d
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2020-07-14 21:09:19 +0530

    Upgrading to java 14, latest gradle and spring boot

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/openjdk-13.jdk/Contents/Home"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/openjdk-14.0.2.jdk/Contents/Home"
 export JAVA_OPTS="-Xms512m -Xmx512m"
 exec ./gradlew "$@"
