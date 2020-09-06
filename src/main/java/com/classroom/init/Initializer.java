c966138a4923d57d2cdfc2ffd67cd5674c3cf996
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2020-09-06 18:09:19 +0530

    hsql ui disabled

--- src/main/java/com/classroom/init/Initializer.java
+++ src/main/java/com/classroom/init/Initializer.java
@@ -1,6 +1,7 @@
 package com.classroom.init;
 
 import org.hsqldb.util.DatabaseManagerSwing;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -11,15 +12,19 @@ import javax.annotation.PostConstruct;
  */
 @Component
 public class Initializer {
+    @Value("${hsql.ui.disabled}")
+    private Boolean isUIDisabled;
 
     /**
      * This is to run a sql manager that comes up with hsqldb, while starting the app.
      */
     @PostConstruct
     public void init() {
-        System.setProperty("java.awt.headless", "false");
-        DatabaseManagerSwing.main(
-                new String[]{"--url", "jdbc:hsqldb:mem:testdb", "--user", "sa", "--password", ""});
+        if (!isUIDisabled) {
+            System.setProperty("java.awt.headless", "false");
+            DatabaseManagerSwing.main(
+                    new String[]{"--url", "jdbc:hsqldb:mem:testdb", "--user", "sa", "--password", ""});
+        }
     }
 
 }
