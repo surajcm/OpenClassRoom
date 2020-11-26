ec77906cb71f7be1117a2c76c89a14706f93fba1
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2020-11-27 04:40:30 +0530

    Upgrading to java 15

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/openjdk-14.0.2.jdk/Contents/Home"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-15.0.1.jdk/Contents/Home"
 export JAVA_OPTS="-Xms512m -Xmx512m"
 exec ./gradlew "$@"
