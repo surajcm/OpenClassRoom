7294e79a13e74280858f9c48eece3cfeabbd9275
Suraj Muraleedharan <surajcm@gmail.com>
2022-01-04 10:32:25 +0530

    Updating to spring boot 2.6.2 and gradle 7.3.2

--- src/main/java/com/classroom/init/WebSecurityConfig.java
+++ src/main/java/com/classroom/init/WebSecurityConfig.java
@@ -4,7 +4,8 @@ import com.classroom.user.service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -41,12 +42,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private String[] getExclusionPaths() {
         return new String[]{
                 "/resources/**",
-                "/css/**", "/js/**", "/img/**",
+                "/css/**",
+                "/js/**",
+                "/img/**",
                 "/registration"};
     }
 
-    @Autowired
-    public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bcryptPasswordEncoder());
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        var provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(bcryptPasswordEncoder());
+        return provider;
     }
 }
