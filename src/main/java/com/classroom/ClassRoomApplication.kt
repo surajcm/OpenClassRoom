8d460b93dae5b08c4913d8d3eafba02f35235adb
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2022-10-26 08:05:45 +0530

    starting to mix java and kotlin

---
+++ src/main/java/com/classroom/ClassRoomApplication.kt
@@ -0,0 +1,11 @@
+package com.classroom;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class ClassRoomApplication
+
+fun main(args: Array<String>) {
+    runApplication<ClassRoomApplication>(*args)
+}
